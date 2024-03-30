77289c4fa7fe59cc8710cd9c54ecf86709cbf5bb
VictoriaKibina <164630018+VictoriaKibina@users.noreply.github.com>
2024-03-30 11:26:48 +0300

    Update zadanie1.py

--- zadanie1.py
+++ zadanie1.py
@@ -3,12 +3,13 @@ a.pop(0)
 for i in range(len(a)):
     a[i]=a[i].strip(',').split('%')
 
+
 for i in range(len(a)):
     r=(str(float(len(a[5])/sum(a[5]))))
+    r=r//100
 
 f=open('books.txt','w',encoding='utf-8')
 f.writelines('books_new.csv')
 for x in a:
     f.writelines(','.join(x))
 f.close()
-
